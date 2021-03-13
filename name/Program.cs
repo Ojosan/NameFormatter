835da50530de7b41043a88ee1d6d6871a1df7d2e
Ojosan <koshka.slytherin@gmail.com>
2021-03-13 18:38:56 -0500

    Name Formatter

---
+++ name/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please fill in the following: ");
+
+            //fill in/userinput
+
+            Console.Write("first name: ");
+            string firstname = Console.ReadLine();
+
+            Console.WriteLine("");
+
+            Console.Write("middle name: ");
+            string middlename = Console.ReadLine();
+
+            Console.WriteLine("");
+
+            Console.Write("last name: ");
+            string lastname = Console.ReadLine();
+
+            Console.WriteLine("");
+
+            Console.Write("title: ");
+            string title = Console.ReadLine();
+
+            //results
+
+            Console.WriteLine("=================================================");
+
+            Console.WriteLine(title +" " +firstname+" " + middlename +" "+ lastname);
+
+            Console.WriteLine(firstname + " " + middlename + " " + lastname);
+
+            Console.WriteLine(firstname + " "+lastname);
+
+            Console.WriteLine(lastname + ", " + firstname + " " + middlename + ", " + title);
+
+            Console.WriteLine(lastname + ", " + firstname + " " + middlename);
+
+            Console.WriteLine(lastname + ", " + firstname);
+
+            //close program
+
+            Console.WriteLine("press any key to close prgram");
+            Console.ReadKey();
+        }
+    }
+}
